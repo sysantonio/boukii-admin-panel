42d0f8105f564afe94036edc5fdb30fc8190ab75
Adrián Rincón López <arincon@aubay.es>
2023-12-28 12:49:08 +0100

    rec pwd

--- src/app/pages/recover-password/recover-password.component.ts
+++ src/app/pages/recover-password/recover-password.component.ts
@@ -64,7 +64,7 @@ export class RecoverPasswordComponent implements OnInit {
 
   send() {
     //this.authService.login(this.form.value.email, this.form.value.password);
-    this.crudService.post('/users', {token: this.token, password: this.form.value.password})
+    this.crudService.recoverPassword('/users', {token: this.token, password: this.form.value.password})
       .subscribe(() => {
         this.snackbar.open(this.translateService.instant('snackbar.password_updated') , 'OK', {duration: 3000});
         this.updated = true;
