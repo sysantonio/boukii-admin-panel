e42b045ab2997b26b7fa85b4a5ab52567db96bf6
ad17427 <andoni.serrano@versia.com>
2024-01-19 12:20:41 +0100

    Environments

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  production: true,
+  production: false,
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  baseUrl: 'https://api.boukii.golembyte.com/api',
+  baseUrl: 'http://api-boukii.test/api',
   firebaseConfig: {
     apiKey: "AIzaSyAVqgEm3-_sMPLqxySQpyHKEfLtQ1_7pHI",
     authDomain: "boukii-test.firebaseapp.com",
