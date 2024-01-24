eae890180b3ecea50be8999a269a7122a29c319a
Adrián Rincón López <arincon@bike-on.app>
2024-01-24 21:51:36 +0100

    qr

--- src/@vex/components/aio-table/aio-table.component.ts
+++ src/@vex/components/aio-table/aio-table.component.ts
@@ -17,9 +17,13 @@ import { FormControl, UntypedFormControl } from '@angular/forms';
 import { ApiCrudService } from 'src/service/crud.service';
 import { MOCK_COUNTRIES } from 'src/app/static-data/countries-data';
 import { MOCK_PROVINCES } from 'src/app/static-data/province-data';
-import moment from 'moment';
+import moment, { duration } from 'moment';
 import { ConfirmModalComponent } from 'src/app/pages/monitors/monitor-detail/confirm-dialog/confirm-dialog.component';
 import { TranslateService } from '@ngx-translate/core';
+
+import { jsPDF } from 'jspdf';
+import * as QRCode from 'qrcode';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @UntilDestroy()
 @Component({
@@ -137,7 +141,7 @@ export class AioTableComponent implements OnInit, AfterViewInit {
   inactiveMonitor = false;
   allMonitors = true;
 
-  constructor(private dialog: MatDialog, private router: Router, private crudService: ApiCrudService, private cdr: ChangeDetectorRef, private translateService: TranslateService) {
+  constructor(private dialog: MatDialog, private router: Router, private crudService: ApiCrudService, private cdr: ChangeDetectorRef, private translateService: TranslateService, private snackbar: MatSnackBar) {
     this.user = JSON.parse(localStorage.getItem('boukiiUser'));
     this.schoolId = this.user.schools[0].id;
   }
@@ -776,4 +780,123 @@ export class AioTableComponent implements OnInit, AfterViewInit {
 
     return {min: min.date, max: max.date}
   }
+
+  /* EXPORT QR */
+
+  hexToRgb(hex:string) {
+    const rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return rgb ? {
+      r: parseInt(rgb[1], 16),
+      g: parseInt(rgb[2], 16),
+      b: parseInt(rgb[3], 16)
+    } : null;
+  }
+
+  exportQR(id:any) {
+    console.log('export');
+    this.crudService.get('/admin/clients/course/'+ id)
+        .subscribe(async (data) => {
+          console.log(data);
+
+          const clientsData = data.data;
+
+          if(clientsData && clientsData.length){
+            const doc = new jsPDF();
+            const pageWidth = doc.internal.pageSize.getWidth();
+            const colWidth = pageWidth / 2;
+            const lineHeight = 6;
+            const qrSize = 48;
+            let y = 10;
+
+            for (let i = 0; i < clientsData.length; i++) {
+              const client = clientsData[i];
+              const isLeftColumn = i % 2 === 0;
+              const baseX = isLeftColumn ? 10 : colWidth + 6;
+              const qrX = baseX + 48;
+              let y_text = y;
+              const maxWidthText = 48;
+
+              doc.setTextColor(70, 70, 70);
+              doc.setFontSize(16);
+              doc.setFont('helvetica', 'bold');
+              let lines = doc.splitTextToSize(`${client.client?.first_name} ${client.client?.last_name}`, maxWidthText);
+              doc.text(lines, baseX, y_text);
+              y_text += (lines.length + 0.4) * lineHeight;
+
+              if(client.client?.phone || client.client?.telephone){
+                let clientPhone = '';
+                if(client.client?.phone){clientPhone = client.client.phone;}
+                else{clientPhone = client.client.telephone;}
+                doc.setFontSize(14);
+                doc.setFont('helvetica', 'normal');
+                lines = doc.splitTextToSize(`${clientPhone}`, maxWidthText);
+                doc.text(lines, baseX, y_text);
+                y_text += lines.length * lineHeight;
+              }
+
+              doc.setFontSize(9);
+              doc.setFont('helvetica', 'normal');
+              lines = doc.splitTextToSize(`${client.course?.name}`, maxWidthText);
+              doc.text(lines, baseX, y_text);
+              y_text += (lines.length * lineHeight) - 2;
+
+              if(client.monitor){
+                doc.setFontSize(8);
+                lines = doc.splitTextToSize(`Professeur - niveau`, maxWidthText);
+                doc.text(lines, baseX, y_text);
+                y_text += (lines.length * lineHeight) - 2;
+                doc.setFontSize(11);
+                doc.setFont('helvetica', 'bold');
+                lines = doc.splitTextToSize(`${client.monitor?.first_name} ${client.monitor?.last_name}`, maxWidthText);
+                doc.text(lines, baseX, y_text);
+                y_text += (lines.length * lineHeight) + 3;
+              }
+              else{
+                y_text += 6;
+              }
+
+              if(client.degree){
+                const rgbColor = this.hexToRgb(client.degree.color);
+                doc.setFillColor(rgbColor.r, rgbColor.g, rgbColor.b);
+                doc.setTextColor(255, 255, 255);
+                doc.setFontSize(9);
+                doc.setFont('helvetica', 'normal');
+
+                const text = `${client.degree?.annotation} - ${client.degree?.name}`;
+                lines = doc.splitTextToSize(text, maxWidthText);
+                const textBoxHeight = (lines.length + 0.5) * lineHeight;
+
+                doc.rect(baseX, y_text - lineHeight, maxWidthText, textBoxHeight, 'F');
+
+                doc.text(lines, baseX + 1.5, y_text);
+                doc.setTextColor(70, 70, 70);
+                y_text += textBoxHeight;
+              }
+
+              // Generate QR code
+              const qrData = await QRCode.toDataURL(client.client.id.toString());
+              doc.addImage(qrData, 'JPEG', qrX, y-10, qrSize, qrSize);
+
+              //Next row if not left and not last
+              if (!isLeftColumn || i === clientsData.length - 1) {
+                y += qrSize + lineHeight * 4;
+              }
+
+              if (y >= doc.internal.pageSize.getHeight() - 20) {
+                doc.addPage();
+                y = 10;
+              }
+            }
+
+            doc.save('clients.pdf');
+          }
+          //No clients
+          else{
+            this.snackbar.open(this.translateService.instant('course_without_clients'), 'OK', {duration: 3000});
+          }
+
+      })
+  }
+
+  /* END EXPORT QR */
 }
