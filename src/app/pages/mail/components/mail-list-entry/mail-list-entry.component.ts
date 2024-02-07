da402d8bc28c6252be96dc6a764d10726111cf3f
Adrián Rincón López <arincon@bike-on.app>
2024-02-07 20:36:13 +0100

    correos

--- src/app/pages/mail/components/mail-list-entry/mail-list-entry.component.ts
+++ src/app/pages/mail/components/mail-list-entry/mail-list-entry.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MailService } from '../../services/mail.service';
+import { MailComposeComponent } from '../mail-compose/mail-compose.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'vex-mail-list-entry',
@@ -16,7 +18,8 @@ export class MailListEntryComponent implements OnInit {
   hovered: boolean;
 
   constructor(private cd: ChangeDetectorRef,
-              private mailService: MailService) { }
+              private mailService: MailService,
+              private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
@@ -53,5 +56,16 @@ export class MailListEntryComponent implements OnInit {
 
   markMailAsRead(mailId: any['id']) {
     this.mailService.markMailAsRead(mailId);
+  }
+
+  openCompose() {
+    const dialog = this.dialog.open(MailComposeComponent, {
+      width: '100%',
+      maxWidth: 600
+    });
+
+    dialog.afterClosed().subscribe((data) => {
+      this.mailService.getData();
+    })
   }
 }
