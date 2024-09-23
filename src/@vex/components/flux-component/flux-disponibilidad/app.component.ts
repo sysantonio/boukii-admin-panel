e62969bfc166a49c5b55864bac223219332d6c0e
Hulala <yang.ye.1@hotmail.com>
2024-09-23 23:19:34 +0200

    Tabla disponibilidad

--- src/@vex/components/flux-component/flux-disponibilidad/app.component.ts
+++ src/@vex/components/flux-component/flux-disponibilidad/app.component.ts
@@ -14,7 +14,7 @@ export class FluxDisponibilidadComponent {
   ISODate = (n: number) => new Date(new Date().getTime() + n * 24 * 60 * 60 * 1000).toISOString()
   displayFn = (value: any): string => value
   ramdonColor = () => Math.random() > 0.5 ? "#2FAA41" : Math.random() > 0.5 ? "#FFCF25" : Math.random() > 0.5 ? '#E70F0F' : '#C7D0D3'
-
+  selectDate: number = 0
 
 }
 
