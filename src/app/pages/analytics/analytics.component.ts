501848c5477539def6195b4cb00235f2fc02d9da
ad17427 <andoni.serrano@versia.com>
2025-02-11 16:42:17 +0100

    Added extras

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -76,6 +76,7 @@ export class AnalyticsComponent implements AfterViewInit, AfterViewChecked {
     { label: 'admin', property: 'admin', type: 'text', visible: true, cssClasses: ['font-medium'] },
     { label: 'web', property: 'web', type: 'text', visible: true, cssClasses: ['font-medium'] },
     { label: 'vouchers', property: 'vouchers', type: 'price', visible: true, cssClasses: ['font-medium'] },
+    { label: 'extras', property: 'extras', type: 'price', visible: true, cssClasses: ['font-medium'] },
     /*{ label: 'gift_vouchers', property: 'sell_voucher', type: 'price', visible: true, cssClasses: ['font-medium'] },*/
     //{label: 'discount_code', property: 'vouchers_gift', type: 'price', visible: true, cssClasses: ['font-medium']},
     { label: 'total', property: 'total_cost', type: 'price', visible: true },
